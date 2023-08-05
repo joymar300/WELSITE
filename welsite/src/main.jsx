f8198e5c50ee25c988d097e9fbd47afec8f1c279
joymar300 <74611985+joymar300@users.noreply.github.com>
2023-08-04 22:27:21 -0500

    a

--- welsite/src/main.jsx
+++ welsite/src/main.jsx
@@ -1,20 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import Navbar from './components/navbar/Navbar.jsx'
-import HomeView from './components/home/HomeView.jsx'
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Navbar/>
-    <HomeView></HomeView>
-
-  </React.StrictMode>,
-)
-
-=======
 import App from './App.jsx'
 import './index.css'
 
@@ -23,4 +8,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
->>>>>>> porsilascago
--- welsite/vite.config.js
+++ welsite/vite.config.js
@@ -1,11 +1,4 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-=======
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -13,5 +6,4 @@ export default defineConfig({
   assetsInclude: ['**/*.png,'],
   plugins: [react()],
    // Agrega esta línea para incluir archivos PNG en los activos.
->>>>>>> porsilascago
 })
