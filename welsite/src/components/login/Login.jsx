516515fca072abbe3c75960ce1cbb5d1043a3b28
cristian20021027 <cristiandecaro7@gmail.com>
2023-08-09 12:33:37 -0500

    Cambios De diseño y mas

---
+++ welsite/src/components/login/Login.jsx
@@ -0,0 +1,33 @@
+import { ReactDOM } from "react";
+
+const Login = ()=>{
+
+    return (
+    <>
+        <div class="form signup_form">
+            <form action="#">
+                <h2>Crear Cuenta</h2>
+
+                <div class="input_box">
+                <input type="email" placeholder="Enter your email" required />
+                <i class="uil uil-envelope-alt email"></i>
+                </div>
+                <div class="input_box">
+                <input type="password" placeholder="Create password" required />
+                <i class="uil uil-lock password"></i>
+                <i class="uil uil-eye-slash pw_hide"></i>
+                </div>
+                <div class="input_box">
+                <input type="password" placeholder="Confirm password" required />
+                <i class="uil uil-lock password"></i>
+                <i class="uil uil-eye-slash pw_hide"></i>
+                </div>
+
+                <button class="button">Crear Cuenta </button>
+
+                <div class="login_signup">Ya tienes Cuenta? <a href="#" id="login">Iniciar Sesion</a></div>
+            </form>
+        </div>
+    </>);
+}
+export default Login
--- welsite/src/layout/Home/Home.jsx
+++ welsite/src/layout/Home/Home.jsx
@@ -1,36 +1,32 @@
 import React from "react";
 import styles from './Home.module.css'; // Importa los estilos como un objeto
-import Image from '../../assets/img/imagen1.png';
+import Logo from '../../assets/img/zorro.png';
+import ClasesComponent from "../Clases/Clases";
+import Card from "../Contenido3/Contenido3";
+import { Link } from 'react-router-dom';
 
 const HomeView = () => {
   return (
     <>
-      <section className={styles.home}>
-        <div className={`${styles.row} container`}>
-          <div className={styles.column}>
-            <h2>BIENVENIDOS A WELTSITE</h2>
-            <p>
-              ¡Bienvenidos a Weltsite! Somos una página para estudiantes de 3ro y
-              4to del Colegio San Alberto Magno. Aquí encontrarás recursos divertidos
-              y lecciones sobre ciencias sociales como historia, geografía y democracia.
-              Únete, participa en actividades y juegos didácticos para mejorar tus conocimientos.
-              ¡Aprende y diviértete en Weltsite!
-            </p>
-            <div className={styles.buttons}>
-              <button className={styles.btn}>Read More</button>
-              <button className={styles.btn}>Contact Us</button>
-            </div>
-          </div>
-          <div className={styles.column}>
-            <img
-              src={Image}
-              alt="heroImg"
-              className={styles.hero_img}
-            />
-          </div>
+      <section className={styles.homepage} id="home">
+      <div className={styles.content}>
+        <div className={styles.text}>
+          <h1>BIENVENIDOS A WELTSITE</h1>
+          <p>
+          ¡Bienvenidos a Weltsite! Somos una página para estudiantes de 3ro y 4to del Colegio San Alberto Magno. <br/>Aquí encontrarás recursos divertidos y lecciones sobre ciencias sociales como historia, geografía y democracia.<br/>Únete, participa en actividades y juegos didácticos para mejorar tus conocimientos.<strong>¡Aprende y diviértete en Weltsite!</strong> </p>
         </div>
-        
-      </section>
+        <div className={styles.buttons}>
+
+        <Link className={styles.btn} to="/contenido3"> {/* Usa Link en lugar de <a> */}
+              Contenido 3
+            </Link>
+            <Link className={styles.btn} to="/contenido3"> {/* Usa Link en lugar de <a> */}
+              Contenido 4
+            </Link>
+
+        </div>
+      </div>
+    </section>
     </>
   );
 };
